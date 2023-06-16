e7ee8975def7c96358183145efdd0fa9fd37fc2d
progIfe <ifeoluwasamson90@gmail.com>
2023-06-16 17:24:04 +0100

    Portfolio Queryset Update

--- folio/views.py
+++ folio/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect
 from .models import About, Skill, Resume, Portfolio, Service
-from django.core.mail import send_mail
-from portfolio import settings
-from django.contrib import messages
 
 # Create your views here.
 
@@ -11,8 +8,8 @@ def home(request):
     abouts = About.objects.all()
     skills = Skill.objects.all().order_by('upload_time')
     resumes = Resume.objects.all()
-    portfolios = Portfolio.objects.all()
-    services = Service.objects.all().order_by('upload_time')
+    portfolios = Portfolio.objects.all().order_by('-upload_time')
+    services = Service.objects.all().order_by('-upload_time')
     context = {
         'abouts': abouts,
         'skills': skills,
