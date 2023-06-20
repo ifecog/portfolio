a0fd0cb764df5f239b8f49f3ea5aebab131a29ce
progIfe <ifeoluwasamson90@gmail.com>
2023-06-20 09:39:37 +0100

    Django Security update

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='mydefaultvalue')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['ifeoluwailori.up.railway.app', '127.0.0:1', 'localhost']
 
